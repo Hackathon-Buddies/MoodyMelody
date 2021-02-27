5d5a94710984336b33ba6aea17613f5aefe7bc82
femisd <femisd@gmail.com>
2021-02-27 21:22:59 +0000

    fixed

--- src/Components/TasteDiveApi/tastediveApi.js
+++ src/Components/TasteDiveApi/tastediveApi.js
@@ -5,64 +5,47 @@ import XMLHttpRequest from 'xmlhttprequest';
 
 function TastediveApi() {
 
-  const [isLoading,setBool] = useState(false);
+  const [isLoading, setBool] = useState(false);
 
-    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+  var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-    const xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
 
-    let tasteDate1 = ['2','3','4'];
-    let tasteDate = [];
+  let tasteDate1 = ['2', '3', '4'];
+  const [recommendations, setRecommendations] = useState([]);
 
-    const setTrue = () =>{
-        setBool(true)
+
+  const fetchAuthorization = () => {
+
+    const url = "https://thingproxy.freeboard.io/fetch/https://tastedive.com/api/similar?q=grandson";
+    xhr.open("GET", url);
+
+    xhr.setRequestHeader("Accept", "application/json");
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        let tasteDate = [];
+        tasteDate = JSON.parse(xhr.responseText).Similar.Results.map(artist => artist.Name)
+        setRecommendations(tasteDate)
+        console.log("a: ", tasteDate)
+      }
     }
-
-
-    const fetchAuthorization = () => {
-             
-        const url = "https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=grandson";
-        xhr.open("GET", url);
-
-        xhr.setRequestHeader("Accept", "application/json");
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-               // console.log(JSON.parse(xhr.responseText)) 
-               // tasteDate.push(JSON.parse(xhr.responseText).Similar.Results)
-                processNames(JSON.parse(xhr.responseText).Similar.Results)
-                }
-            }
-            xhr.send();
-            
-            
-            setTrue()
-
-        }
-
-
-        const processNames = (names) =>{
-          tasteDate.splice(0, tasteDate.length)
-
-          names.forEach(response => {
-            tasteDate.push(response.Name)
-          });
-
-        }
-
-      
-    return (
-      <div>
-
-        <button onClick={() => fetchAuthorization()}> true </button>
-
-        {isLoading && tasteDate.length>0 ?  tasteDate.map(artist =>{
-          <p> {artist}</p>
-          }):<p>nothing</p>}
-
-      </div>
-    );
+    xhr.send();
   }
-  
-  export default TastediveApi;
+  const recommendationList = recommendations.map(rec => <li>{rec}</li>);
+
+  return (
+    <div>
+
+
+      <button onClick={() => fetchAuthorization()}> true </button>
+      <div>
+        <ul>{recommendationList}</ul>
+      </div>
+
+    </div>
+  );
+}
+
+export default TastediveApi;
 
