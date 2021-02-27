ebc81eaa3634c5212b7b16299aa88ba89ce2f1d7
Johnny-98 <petiodjonov@yahoo.com>
2021-02-27 13:09:25 +0000

    Added Routing
    did it with --save

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Navbar from './Components/NavBar/Navbar';
+import LyricsAPI from './Components/LyricsApi/lyricsApi'
 
 function App() {
   return (
     <div className="App">
-      <div>
-        {/* <h1>Welcome to Moody Melody</h1> */}
-        <Navbar />
-      </div>
+      <Router>
+        <>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/hi" component={LyricsAPI}>
+              </Route>
+            </Switch>
+            
+          </div>
+        </>
+      </Router>
     </div>
   );
 }
--- src/Components/LyricsApi/lyricsApi.js
+++ src/Components/LyricsApi/lyricsApi.js
@@ -1,1 +1,15 @@
 // https://developer.musixmatch.com/documentation/api-reference/track-search
+
+import React, { Component } from 'react';
+
+class lyricsApi extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello</h1>
+            </div>
+        );
+    }
+}
+
+export default lyricsApi;
