48a92d25bfec37e0bbb7b7cdae70fb0f319c48b8
femisd <femisd@gmail.com>
2021-02-27 18:37:03 +0000

    spotify init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Spotify from './Components/SpotifyApi/spotifyApi';
+import React, { useState } from 'react';
+
 
 function App() {
+  let topSongs = []
+  const topSongsHandler = songList => topSongs = songList;
   return (
     <div className="App">
       <h1>Welcome to Moody Melody</h1>
+      <Spotify getTopSongs = {topSongsHandler}/>
+      <button onClick={() => console.log(topSongs)}>get top songs</button>
     </div>
   );
 }
