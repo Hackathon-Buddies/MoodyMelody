93644aea138a1924741779a65006e083052f7916
acv-98 <andrei.vanvu@outlook.com>
2021-02-28 04:07:41 +0000

    Lyrics on load working

--- src/App.js
+++ src/App.js
@@ -2,18 +2,45 @@ import './App.css';
 import TastediveApi from './Components/TasteDiveApi/tastediveApi.js';
 import Spotify from './Components/SpotifyApi/spotifyApi';
 import SentimentApi from './Components/SentimentApi/sentimentApi';
-import React, {useState, useEffect } from 'react';
+import React from 'react';
+import axios from "axios";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Navbar from './Components/NavBar/Navbar';
 import LyricsAPI from './Components/LyricsApi/lyricsApi'
 
 function App() {
-  let topSongs = []
+  let topSongs = [];
+  let topLyrics = [];
   const topSongsHandler = songList => {
     topSongs = songList;
     localStorage.setItem('topSongs', JSON.stringify(songList));
+    topLyrics = [];
+    searchList();
+    localStorage.setItem('topLyrics', JSON.stringify(topLyrics))
   }
+  let i = 0;
+
+  const searchList = async () => {
+    const t = topSongs[i].title;
+    const a = topSongs[i].artist
+    axios({
+      url: `https://thingproxy.freeboard.io/fetch/https://api.lyrics.ovh/v1/${a}/${t}`,
+      method: 'GET',
+      timeout: 1000
+    }).catch((err) => console.log(err)).then(s => {
+      if (s) {
+        console.log("this is the res ", s);
+      }
+      topLyrics.push(s ? s : null);
+      console.log("list so far ", topLyrics);
+      if (i < topSongs.length - 1) {
+        i++;
+        searchList();
+      }
+    })
+  };
+
   return (
     <Router>
       <React.Fragment>
--- src/Components/LyricsApi/lyricsApi.js
+++ src/Components/LyricsApi/lyricsApi.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const LyricsApi = (props) => {
+const LyricsApi = () => {
     const [artist, setArtist] = useState('');
     const [song, setSong] = useState('');
     const [lyrics, setLyrics] = useState('');
@@ -11,36 +11,12 @@ const LyricsApi = (props) => {
             const songText = song === '' ? '- No song was given' : 'for ' + song;
             const artistText = artist === '' ? '- No artist was given' : 'by ' + artist;
             setLyrics(`Lyrics not found ${songText} ${artistText}`);
-        },5000)
+        },1000)
         const link = `https://api.lyrics.ovh/v1/${artist}/${song}`
         axios.get(link).then(res => {
             clearTimeout(lyricsNotFound);
             setLyrics(res.data.lyrics);
         });
-    }
-
-
-    let lyricsList = []
-    let i = 0;
-
-
-    const searchList = async () => {
-        const songs = JSON.parse(localStorage.getItem("topSongs"));
-        console.log(songs)
-        const t = songs[i].title;
-        const a = songs[i].artist
-        axios({url: `https://thingproxy.freeboard.io/fetch/https://api.lyrics.ovh/v1/${a}/${t}`, method:'GET'}).catch((err) => console.log(err)).then(s => {
-                console.log("this is the res ", s);
-                lyricsList.push(s);
-                console.log("list so far ", lyricsList);
-            if (i < 19) {
-                i++;
-                searchList();
-            }
-
-
-        })
-
     }
 
     return (
@@ -77,7 +53,6 @@ const LyricsApi = (props) => {
                 Get Track Lyrics
             </button>
             <textarea disabled value={lyrics} cols="30" rows="10"></textarea>
-            <button onClick={searchList}>New test</button>
         </div>
     );
 }
