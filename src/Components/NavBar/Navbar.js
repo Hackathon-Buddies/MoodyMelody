323d17f53f94a8a7ed8239411a8ab99a9a885b81
Johnny-98 <petiodjonov@yahoo.com>
2021-02-28 02:55:29 +0000

    Im at my limit with this navbar

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
 import logo from '../../styles/logo.png'
-import* as ReactBootstrap from 'react-bootstrap'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-dark bg-dark mb-5">
-      <span className="navbar-brand mb-0 h1 "><img src={logo} alt="Welcome to Moody Melody" width="70" height="70"/>Moody Melody </span>
-      <Link  to="/lyrics">Find Lyrics</Link>
-      <Link  to="/">Homepage</Link>
+    <nav className="navbar navbar-dark bg-dark ">
+      <div><span className="navbar-brand mb-0 h1 "><img src={logo} alt="Welcome to Moody Melody" width="70" height="70" />Moody Melody </span></div>
+      <div><a href="/lyrics" className="navbar-item">Find Lyrics</a></div>
+      <div><a href="/" className="navbar-item">Homepage</a></div>
     </nav>
   );
 };
