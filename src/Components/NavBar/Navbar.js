965127ec70bc136ca6e018900542905bd464acc6
Johnny-98 <petiodjonov@yahoo.com>
2021-02-28 02:30:30 +0000

    Added a bad navbar

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import logo from '../../styles/logo.png'
+import* as ReactBootstrap from 'react-bootstrap'
 
 const Navbar = () => {
   return (
-      
     <nav className="navbar navbar-dark bg-dark mb-5">
-      <span className="navbar-brand mb-0 h1 mx-auto">Welcome to Moody Melody</span>
+      <span className="navbar-brand mb-0 h1 "><img src={logo} alt="Welcome to Moody Melody" width="70" height="70"/>Moody Melody </span>
+      <Link  to="/lyrics">Find Lyrics</Link>
+      <Link  to="/">Homepage</Link>
     </nav>
   );
 };
