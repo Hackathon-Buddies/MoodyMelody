0781f79ef9daeeec3ebd7be663c3d9c111d75cc0
acv-98 <andrei.vanvu@outlook.com>
2021-02-28 00:56:48 +0000

    First test to find lyrics by list of songs

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 import TastediveApi from './Components/TasteDiveApi/tastediveApi.js';
 import Spotify from './Components/SpotifyApi/spotifyApi';
 import SentimentApi from './Components/SentimentApi/sentimentApi';
-
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Navbar from './Components/NavBar/Navbar';
@@ -12,23 +11,28 @@ import LyricsAPI from './Components/LyricsApi/lyricsApi'
 function App() {
   let topSongs = []
   const topSongsHandler = songList => topSongs = songList;
+
+  useEffect(() => {
+    if (Array.isArray(topSongs) && topSongs.length > 0){
+      localStorage.setItem('topSongs', JSON.stringify(topSongs));
+    }
+  }, [topSongs])
+
   return (
-
-      <Router>
-        <React.Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/lyrics">{<LyricsAPI />}</Route>
-              <Route exact path="/recommendation"><TastediveApi /></Route>
-              <Route exact path="/"><Spotify getTopSongs={topSongsHandler} /></Route>
-              <React exact path="sentiment"><SentimentApi /></React>
-            </Switch>
-
-            <button onClick={() => console.log(topSongs)}>get top songs</button>
-          </div>
-        </React.Fragment>
-      </Router>
+    <Router>
+      <React.Fragment>
+        <Navbar />
+        <Spotify getTopSongs={topSongsHandler} />
+        <div className="container">
+          <Switch>
+            <Route exact path="/lyrics">{<LyricsAPI />}</Route>
+            <Route exact path="/recommendation"><TastediveApi /></Route>
+            <React exact path="sentiment"><SentimentApi /></React>
+          </Switch>
+          <button onClick={() => console.log(topSongs)}>get top songs</button>
+        </div>
+      </React.Fragment>
+    </Router>
   );
 }
 
--- src/Components/LyricsApi/lyricsApi.js
+++ src/Components/LyricsApi/lyricsApi.js
@@ -1,11 +1,74 @@
-import React from "react";
-import Search from './Search';
+import React, { useState } from "react";
+import axios from "axios";
 
-const lyricsApi = () => {
+const LyricsApi = (props) => {
+    const [artist, setArtist] = useState('');
+    const [song, setSong] = useState('');
+    const [lyrics, setLyrics] = useState('');
+    
+
+    const onSearch = () => {
+        const lyricsNotFound = setTimeout(() => {
+            const songText = song === '' ? '- No song was given' : 'for ' + song;
+            const artistText = artist === '' ? '- No artist was given' : 'by ' + artist;
+            setLyrics(`Lyrics not found ${songText} ${artistText}`);
+        },5000)
+        const link = `https://api.lyrics.ovh/v1/${artist}/${song}`
+        axios.get(link).then(res => {
+            clearTimeout(lyricsNotFound);
+            setLyrics(res.data.lyrics);
+        });
+    }
+
+    const searchList = async () => {
+        const songs = JSON.parse(localStorage.getItem("topSongs"));
+        const allLyrics = songs.map(song => {
+            const lyric = axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`)
+                .then(res => res)
+                .catch(err => err)
+            return lyric;
+        })
+        const result = await Promise.all(allLyrics);
+        return result;
+    }
+
     return (
-      <React.Fragment>
-        <Search />
-      </React.Fragment>
+        <div className="card card-body mb-4 p-4">
+            <h1 className="display-4 text-center">
+                <i className="fas fa-music" /> Search For A Song
+                </h1>
+
+            <p className="lead text-center">And I will grade your shitty taste</p>
+
+            <div className="form-group">
+                <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Song title..."
+                    name="songInput"
+                    value={song}
+                    onChange={e => setSong(e.target.value)}
+                />
+            </div>
+
+            <div className="form-group">
+                <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Artist..."
+                    name="artistInput"
+                    value={artist}
+                    onChange={e => setArtist(e.target.value)}
+                />
+            </div>
+
+            <button className="btn btn-primary btn-lg btn-block mb-5" onClick={() =>onSearch(song, artist)}>
+                Get Track Lyrics
+            </button>
+            <textarea disabled value={lyrics} cols="30" rows="10"></textarea>
+            <button onClick={searchList}>New test</button>
+        </div>
     );
-  };
-  export default lyricsApi;
+}
+
+export default LyricsApi;
